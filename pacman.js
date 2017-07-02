6ded23a7c15b6c85d4d82bd9e92d7fb206d0485a
Ronen Annason <ronenannason83@gmail.com>
2017-07-02 17:52:03 -0400

    added ghost variables to pacman game

--- pacman.js
+++ pacman.js
@@ -85,3 +85,35 @@ stdin.on('data', function(key) {
 process.on('exit', function() {
   console.log('\n\nGame Over!\n');
 });
+
+var inky = {
+  menu_option: '1',
+  name: 'Inky',
+  colour: 'Red',
+  character: 'Shadow',
+  edible: false
+};
+
+var blinky = {
+  menu_option: '2',
+  name: 'Blinky',
+  colour: 'Cyan',
+  character: 'Speedy',
+  edible: false
+};
+
+var pinky = {
+  menu_option: '3',
+  name: 'Pinky',
+  colour: 'Pink',
+  character: 'Bashful',
+  edible: false
+};
+
+var clyde = {
+  menu_option: '4',
+  name: 'clyde',
+  colour: 'Orange',
+  character: 'Pokey',
+  edible: false
+};
