34a89b75decb7e88fe5120553ff58edc5748a8b3
Ronen Annason <ronenannason83@gmail.com>
2017-07-04 18:59:50 -0400

    working on saving highscore functionality

--- pacman.js
+++ pacman.js
@@ -2,7 +2,28 @@
 var score = 0;
 var lives = 2;
 var powerPellets = 4;
-var dots = 240
+var dots = 240;
+var ghostEaten = 0;
+var level = 1;
+var valid = 0;
+var name='';
+var times=0;
+
+var bonus = {
+  1: ['cherry', 100],
+  2: ['strawberry', 300],
+  3: ['orange', 500],
+  4: ['orange', 500],
+  5: ['apple', 700],
+  6: ['apple', 700],
+  7: ['pineapple', 1000],
+  8: ['pineapple', 1000],
+  9: ['galaxian_spaceship', 2000],
+  10: ['galaxian_spaceship', 2000],
+  11: ['bell', 3000],
+  12: ['bell', 3000],
+  13: ['key', 5000]
+}
 
 var inky = {
   menu_option: '1',
@@ -39,6 +60,7 @@ var clyde = {
 var ghosts = [inky, blinky, pinky, clyde]
 
 
+
 // Define your ghosts here
 
 // replace this comment with your four ghosts setup as objects
@@ -46,12 +68,16 @@ var ghosts = [inky, blinky, pinky, clyde]
 
 // Draw the screen functionality
 function drawScreen() {
+  if (times >=3){
   clearScreen();
   setTimeout(function() {
     displayStats();
     displayMenu();
     displayPrompt();
-  }, 10);
+  }, 10);}
+  else {
+displayPromptforname();
+  }
 }
 
 function clearScreen() {
@@ -59,11 +85,23 @@ function clearScreen() {
 }
 
 function displayStats() {
-  console.log('Score: ' + score + '     Lives: ' + lives +     '    Dots: ' + dots +  '\n\n\nPower-Pellets:' + powerPellets );
+  console.log('Score: ' + score + '     Lives: ' + lives +     '    Dots: ' + dots +  '\nLevel: ' + level +  '\nName: '+ name+'\nPower-Pellets:' + powerPellets );
+
 }
 
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
+ valid = 0
+  var random = Math.floor((Math.random() * 5) + 1);
+  if(random == 3) {
+    var index = level
+    if(level > 13) {
+      index=13
+    }
+    console.log('\n======Yay! here is your bonus=====');
+    console.log('(m) Eat ' + bonus[index][0]+' for bonus');
+    valid =1
+  }
   console.log('(d) Eat Dot');
   console.log('(e) Eat 10 Dots');
   console.log('(f) Eat 100 Dots');
@@ -81,6 +119,8 @@ function displayPrompt() {
   // process.stdout.write is similar to console.log except it doesn't add a new line after the text
   process.stdout.write('\nWaka Waka :v '); // :v is the Pac-Man emoji.
 }
+
+
 
 
 // Menu Options
@@ -101,6 +141,7 @@ function eatDot(num) {
 } else {
   console.log("\nno more dots for you!")
 }
+newLevel()
 }
 
 function amIEdible(ghost) {
@@ -119,6 +160,7 @@ function eatPowerPellet() {
   console.log('\nYummy!');
   score += 50;
   powerPellets -= 1;
+  newLevel();
   for (var index = 0; index < ghosts.length; index++) {
   ghosts[index].edible = true;
  }
@@ -134,15 +176,62 @@ function eatGhost(ghost){
     console.log("\n " + ghost.name + "was yummy!");
     ghost.edible = false
     lives += 0
-    score += 200
+    if (ghostEaten == 0) {
+    score += 200;
+  } else if (ghostEaten == 1) {
+    score += 400;
+  } else if (ghostEaten == 2) {
+    score += 800;
+  } else {
+    score += 1600;
+  }
+    ghostEaten += 1
   }
   }
+
+  function newLevel(){
+    if (level == 256) {
+      console.log('\nCongrats, you have no life, but you win PACMAN YEY!');
+      saveHighScore();
+      process.exit();
+    } else {
+      if ((powerPellets == 0) && (dots == 0)) {
+      level += 1;
+      dots = 240;
+      powerPellets =4;
+    }
+  }
+}
+
+function eatBonus() {
+  var index = level
+  if (level > 13){
+    index = 13;
+
+  }
+  score += bonus[index][1]
+}
+
+
+
 //game over function not working yet.
 function gameOver() {
   if (lives <= 0) {
+    saveHighScore();
     process.exit();
     console.log('\n Game Over');
   }
+}
+
+function saveHighScore(){
+  var fs = require('fs');
+var stream = fs.createWriteStream("my_file.txt");
+stream.once('open', function(fd) {
+  stream.write("My first row\n");
+  stream.write("My second row\n");
+  stream.end();
+});
+
 }
 
 
@@ -162,6 +251,10 @@ function processInput(key) {
       break;
     case 'd':
       eatDot(1);
+      break;
+    case 'm':
+      if (valid==1){
+      eatBonus();}
       break;
     case 'e':
       eatDot(10);
@@ -206,13 +299,23 @@ stdin.setRawMode(true);
 stdin.resume();
 stdin.setEncoding('utf8');
 
+
+function displayPromptforname() {
+  // process.stdout.write is similar to console.log except it doesn't add a new line after the text
+  process.stdout.write('\nEnter your '+times+' initials : '); // :v is the Pac-Man emoji.
+}
 // Draw screen when game first starts
+
 drawScreen();
 
 // Process input and draw screen each time player enters a key
 stdin.on('data', function(key) {
   process.stdout.write(key);
-  processInput(key);
+  if (times < 3){
+    name +=key
+    times +=1
+  }else{
+  processInput(key);}
   setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
 });
 
